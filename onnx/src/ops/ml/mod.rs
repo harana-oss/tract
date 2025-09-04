148bef006b9619dce25f90ce4c36186b3e29aa3b
Naden <naden@harana.com>
2025-09-04 11:22:44 +1000

    Added ONNX ML LinearRegressor operation

--- onnx/src/ops/ml/mod.rs
+++ onnx/src/ops/ml/mod.rs
@@ -1,11 +1,11 @@
 mod category_mapper;
-mod normalizer;
+mod linear_regressor;
 mod tree_ensemble_classifier;
 
 use crate::model::OnnxOpRegister;
 
 pub fn register_all_ops(reg: &mut OnnxOpRegister) {
     category_mapper::register_all_ops(reg);
-    normalizer::register_all_ops(reg);
+    linear_regressor::register_all_ops(reg);
     tree_ensemble_classifier::register_all_ops(reg);
 }
